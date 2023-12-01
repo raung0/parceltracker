1ad4b837d354c5df8f6b27280bfe128056a572bf
Ungureanu Radu-Andrei <radu.ungureanu@socopon.com>
2023-12-01 21:44:37 +0200

    Print an error message if a parcel could not be found
    Signed-off-by: Ungureanu Radu-Andrei <radu.ungureanu@socopon.com>

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     tracking_number = sys.argv[1]
 
     parcel_info = base.get_parcel_info(tracking_number)
+    if parcel_info.to == '':
+        print("No parcel info found for tracking number: {}".format(tracking_number))
+        return
+
     print(parcel_info.pretty_print())
 
 if __name__ == "__main__":
